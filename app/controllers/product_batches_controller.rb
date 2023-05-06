00f2924b6746b637f86dbff04f7cd75a87ddfed3
Luqui <luizquintanilha1987@gmail.com>
2023-05-06 13:32:15 -0300

    correção do bug nos testes

--- app/controllers/product_batches_controller.rb
+++ app/controllers/product_batches_controller.rb
@@ -1,6 +1,6 @@
 class ProductBatchesController < ApplicationController
   before_action :authenticate_admin!, only: [:index, :new, :create, :show]
-  before_action :authenticate_user!, only: [:index, :show]
+  #before_action :authenticate_user!, only: [:index, :show]
   def index
     @product_batches = ProductBatch.all
   end
@@ -26,20 +26,6 @@ class ProductBatchesController < ApplicationController
     @product_batch = ProductBatch.find(params[:id])
     @product = Product.find(params[:id])
     @products = Product.all
-  end
-
-  def new_item
-    @product_batch = ProductBatch.new
-    @products = Product.all
-    @product = Product.find(params[:id])
-  end
-
-  def create_item
-    @product_batch = ProductBatch.new(product_batch_params)
-    if @product_batch.save
-      flash[:notice] = 'Item foi incluso ao lote.'
-      redirect_to @product_batch
-    end
   end
 
   private
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_admin!, only: [:index, :new, :create, :show, :edit, :update]
-  before_action :authenticate_user!, only: [:index, :show]
+  #before_action :authenticate_user!, only: [:index, :show]
   def index 
     @products = Product.all
   end
